90fd55e57ff988586c39f3effd2d89e8340b2e3a
Danylo Kushch <107958837+NotJediYet@users.noreply.github.com>
2022-07-06 18:09:08 +0300

    Create Startup and update Program (#8)

--- src/SportsHub.Web/Program.cs
+++ src/SportsHub.Web/Program.cs
@@ -1,31 +1,17 @@
-using SportsHub.Extensions;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddBusiness();
-builder.Services.AddInfrastructure();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+namespace SportsHub.Web
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseCors(builder => builder
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .SetIsOriginAllowed((host) => true)
-        .AllowCredentials());
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
---
+++ src/SportsHub.Web/Startup.cs
@@ -0,0 +1,62 @@
+﻿using SportsHub.Extensions;
+
+namespace SportsHub.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBusiness();
+            services.AddInfrastructure();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            if (Environment.IsDevelopment())
+            {
+                services.AddSwaggerGen();
+                services.AddCors();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(swagger =>
+                {
+                    swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "SportsHub API V1");
+                });
+
+                app.UseCors(builder => builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowCredentials());
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
