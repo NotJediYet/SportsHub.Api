34c36436b3f48e9267130502f185054c30f9e7c9
foxerest <kuliantaras@gmail.com>
2022-07-19 17:48:13 +0300

    connect okta, create test secured get request

---
+++ src/SportsHub.Web/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SportsHub.Web.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [Authorize]
+        [HttpGet("secured-get")]
+        public IActionResult TestSecuredGet()
+        {
+            return Ok(new { Massage = "Here is some private info!" });
+        }
+    }
+}
--- src/SportsHub.Web/Startup.cs
+++ src/SportsHub.Web/Startup.cs
@@ -1,4 +1,6 @@
-﻿using SportsHub.Extensions;
+﻿using Microsoft.OpenApi.Models;
+using Okta.AspNetCore;
+using SportsHub.Extensions;
 
 namespace SportsHub.Web
 {
@@ -19,12 +21,48 @@ namespace SportsHub.Web
             services.AddBusiness();
             services.AddInfrastructure();
 
+            services.AddAuthentication(OktaDefaults.ApiAuthenticationScheme)
+                .AddOktaWebApi(new OktaWebApiOptions()
+                {
+                    OktaDomain = Configuration["Okta:Domain"],
+                    AuthorizationServerId = Configuration["Okta:AuthorizationServerId"]
+                });
+
+
+            services.AddAuthorization();
+
             services.AddControllers();
             services.AddEndpointsApiExplorer();
 
             if (Environment.IsDevelopment())
             {
-                services.AddSwaggerGen();
+                services.AddSwaggerGen(option =>
+                {
+                    option.SwaggerDoc("v1", new OpenApiInfo { Title = "SportsHub API V1", Version = "v1" });
+                    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Description = "Please enter a valid token",
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "Bearer"
+                    });
+                    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            new string[]{}
+                        }
+                    });
+                });
                 services.AddCors();
             }
         }
@@ -51,6 +89,7 @@ namespace SportsHub.Web
             app.UseHttpsRedirection();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
