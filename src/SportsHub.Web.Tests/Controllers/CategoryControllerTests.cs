e4c5223122971c8af4db2170e2846f6662776801
Danylo Kushch <danylo_kushch@outlook.com>
2022-08-04 13:40:41 +0300

    Add Test Example

---
+++ src/SportsHub.Web.Tests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsHub.Business.Services.Abstraction;
+using SportsHub.Shared.Models;
+using SportsHub.Web.Controllers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SportsHub.Web.Tests.Controllers
+{
+    public class CategoryControllerTests
+    {
+        private readonly Mock<ICategoryService> _service;
+        private readonly CategoryController _controller;
+
+        public CategoryControllerTests()
+        {
+            _service = new Mock<ICategoryService>();
+
+            _controller = new CategoryController(_service.Object);
+        }
+
+        [Fact]
+        public async Task CreateCategoryAsync_HasValidValues_ReturnsOkResult()
+        {
+            // Arrange
+            var category = new Category
+            {
+                Name = "Name",
+            };
+
+            // _service.Setup(service => service.CreateAsync(category))
+            //     .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.CreateCategoryAsync(category);
+
+            // Assert
+            var objectResult = Assert.IsType<OkResult>(result);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+    }
+}
