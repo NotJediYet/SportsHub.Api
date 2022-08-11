914f3df48982192702ef7dca8c9cb04f2483247a
Vitalii P <pelekhvitaliy@gmail.com>
2022-08-11 16:39:46 +0300

    add Logo table, model, service, repository and controller, also create "add logo" task and update team by adding location field

--- src/SportsHub.Infrastructure/Repositories/TeamRepository.cs
+++ src/SportsHub.Infrastructure/Repositories/TeamRepository.cs
@@ -61,5 +61,12 @@ namespace SportsHub.Infrastructure.Repositories
 
             await _context.SaveChangesAsync();
         }
+
+        public async Task<bool> DoesTeamAlreadyExistByIdAsync(Guid id)
+        {
+            var teams = await _context.Set<Team>().ToListAsync();
+
+            return teams.Any(team => team.Id == id);
+        }
     }
 }
---
+++ src/SportsHub.Shared/Models/CreateLogoModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SportsHub.Shared.Models
+{
+    public class CreateLogoModel
+    {
+        public byte[] Bytes { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string FileExtension { get; set; }
+        public Guid TeamId { get; set; }
+    }
+}
