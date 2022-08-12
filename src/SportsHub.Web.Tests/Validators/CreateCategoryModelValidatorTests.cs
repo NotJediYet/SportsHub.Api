9bff31da54d5f249050d1c5f874874408741e31b
Danylo Kushch <danylo_kushch@outlook.com>
2022-08-12 17:35:12 +0300

    CreateCategoryModelValidatorTests Refactoring

--- src/SportsHub.Web.Tests/Validators/CreateCategoryModelValidatorTests.cs
+++ src/SportsHub.Web.Tests/Validators/CreateCategoryModelValidatorTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using SportsHub.Business.Services;
 using SportsHub.Shared.Models;
+using SportsHub.Shared.Resources;
 using SportsHub.Web.Validators;
 using Xunit;
 
@@ -19,40 +20,62 @@ namespace SportsHub.Web.Tests.Validators
         }
 
         [Fact]
-        public async void CreateCategoryModel_HasEmptyName_ReturnsValidationResultWithError()
+        public async void CreateCategoryModel_WhenNameIsEmpty_ReturnsValidationResultWithError()
         {
             // Arrange
             var category = new CreateCategoryModel
             {
                 Name = string.Empty
             };
+            var expectedErrorMessage = Errors.CategoryNameCannotBeEmpty;
 
             // Act
             var result = await _validator.ValidateAsync(category);
 
             // Assert
-            Assert.IsType<ValidationResult>(result); 
             Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.ErrorMessage == expectedErrorMessage);
         }
 
         [Fact]
-        public async void CreateCategoryModel_HasExistingName_ReturnsValidationResultWithError()
+        public async void CreateCategoryModel_WhenNameIsNotUnique_ReturnsValidationResultWithError()
         {
             // Arrange
             var category = new CreateCategoryModel
             {
-                Name = "Test name"
+                Name = "Name"
             };
+            var expectedErrorMessage = Errors.CategoryNameIsNotUnique;
 
-            _service.Setup(service => service.DoesCategoryAlreadyExistByNameAsync(It.IsAny<string>()))
+            _service.Setup(service => service.DoesCategoryAlreadyExistByNameAsync(category.Name))
                 .ReturnsAsync(true);
 
             // Act
             var result = await _validator.ValidateAsync(category);
 
             // Assert
-            Assert.IsType<ValidationResult>(result);
             Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.ErrorMessage == expectedErrorMessage);
+        }
+
+        [Fact]
+        public async void CreateCategoryModel_WhenModelIsValid_ReturnsSuccessValidationResult()
+        {
+            // Arrange
+            var category = new CreateCategoryModel
+            {
+                Name = "Name"
+            };
+
+            _service.Setup(service => service.DoesCategoryAlreadyExistByNameAsync(category.Name))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _validator.ValidateAsync(category);
+
+            // Assert
+            Assert.True(result.IsValid);
+            Assert.Empty(result.Errors);
         }
     }
 }
