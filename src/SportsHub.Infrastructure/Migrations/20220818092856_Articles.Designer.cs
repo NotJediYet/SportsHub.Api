44dbed5f533925d04e25db476f924df790978d99
nadiiakhasyshyn <nadiia.khasyshyn.pm.2020@lpnu.ua>
2022-08-18 12:30:25 +0300

    New migration

--- src/SportsHub.Infrastructure/Migrations/20220818092856_Articles.Designer.cs
+++ src/SportsHub.Infrastructure/Migrations/20220818092856_Articles.Designer.cs
@@ -12,8 +12,8 @@ using SportsHub.Infrastructure.DBContext;
 namespace SportsHub.Infrastructure.Migrations
 {
     [DbContext(typeof(SportsHubDbContext))]
-    [Migration("20220816203542_Article")]
-    partial class Article
+    [Migration("20220818092856_Articles")]
+    partial class Articles
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -66,6 +66,26 @@ namespace SportsHub.Infrastructure.Migrations
                     b.ToTable("Articles");
                 });
 
+            modelBuilder.Entity("SportsHub.Shared.Entities.ArticleImage", b =>
+                {
+                    b.Property<byte[]>("Bytes")
+                        .HasColumnType("varbinary(900)");
+
+                    b.Property<Guid>("ArticleId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("FileExtension")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Bytes");
+
+                    b.HasIndex("ArticleId")
+                        .IsUnique();
+
+                    b.ToTable("Images");
+                });
+
             modelBuilder.Entity("SportsHub.Shared.Entities.Category", b =>
                 {
                     b.Property<Guid>("Id")
@@ -87,29 +107,6 @@ namespace SportsHub.Infrastructure.Migrations
                         .IsUnique();
 
                     b.ToTable("Categories");
-                });
-
-            modelBuilder.Entity("SportsHub.Shared.Entities.ArticleImage", b =>
-                {
-                    b.Property<byte[]>("Bytes")
-                        .HasColumnType("varbinary(900)");
-
-                    b.Property<Guid>("ArticleId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<string>("FileExtension")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<decimal>("ImageSize")
-                        .HasColumnType("decimal(18,2)");
-
-                    b.HasKey("Bytes");
-
-                    b.HasIndex("ArticleId")
-                        .IsUnique();
-
-                    b.ToTable("Images");
                 });
 
             modelBuilder.Entity("SportsHub.Shared.Entities.Subcategory", b =>
