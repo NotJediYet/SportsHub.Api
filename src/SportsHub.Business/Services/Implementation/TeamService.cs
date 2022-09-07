c4d3b485ca9234d04772c9e0702bb71a932e67da
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2022-09-07 11:47:08 +0300

    fix build fail

--- src/SportsHub.Business/Services/Implementation/TeamService.cs
+++ src/SportsHub.Business/Services/Implementation/TeamService.cs
@@ -40,12 +40,5 @@ namespace SportsHub.Business.Services
 
             return result;
         }
-
-        public async Task<bool> DoesTeamAlreadyExistByIdAsync(Guid id)
-        {
-            var result = await _teamRepository.DoesTeamAlreadyExistByIdAsync(id);
-
-            return result;
-        }
     }
 }
