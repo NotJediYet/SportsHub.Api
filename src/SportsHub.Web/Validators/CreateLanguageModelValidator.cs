8a44e663eb5f5c3c7bf7d014e8784cb6fb8289f4
markoparfeniuk <marko.parfeniuk.pm.2020@lpnu.ua>
2022-09-23 14:43:41 +0300

    add language validators

---
+++ src/SportsHub.Web/Validators/CreateLanguageModelValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using SportsHub.Business.Services;
+using SportsHub.Shared.Models;
+using SportsHub.Shared.Resources;
+
+namespace SportsHub.Web.Validators
+{
+    public class CreateLanguageModelValidator : AbstractValidator<CreateLanguageModel>
+    {
+        private readonly ILanguageService _languageService;
+
+        public CreateLanguageModelValidator(ILanguageService languageService)
+        {
+            _languageService = languageService ?? throw new ArgumentNullException(nameof(languageService));
+
+            RuleFor(language => language.Name)
+                .NotEmpty().WithMessage(Errors.LanguageNameCannotBeEmpty)
+                .MustAsync((name, cancellation) => DoesLanguageNameIsUniqueAsync(name))
+                .WithMessage(Errors.LanguageNameIsNotUnique);
+
+
+            RuleFor(language => language.Code)
+                .NotEmpty().WithMessage(Errors.LanguageCodeCannotBeEmpty)
+                .MustAsync((code, cancellation) => DoesLanguageCodeIsUniqueAsync(code))
+                .WithMessage(Errors.LanguageCodeIsNotUnique);
+        }
+
+        private async Task<bool> DoesLanguageNameIsUniqueAsync(string languageName)
+        {
+            var result = await _languageService.DoesLanguageAlreadyExistByNameAsync(languageName);
+
+            return !result;
+        }
+
+        private async Task<bool> DoesLanguageCodeIsUniqueAsync(string languageCode)
+        {
+            var result = await _languageService.DoesLanguageAlreadyExistByCodeAsync(languageCode);
+
+            return !result;
+        }
+    }
+}
---
+++ src/SportsHub.Web/Validators/EditLanguageModelValidator.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation;
+using SportsHub.Business.Services;
+using SportsHub.Shared.Models;
+using SportsHub.Shared.Resources;
+
+namespace SportsHub.Web.Validators
+{
+    public class EditLanguageModelValidator : AbstractValidator<EditLanguageModel>
+    {
+        private readonly ILanguageService _languageService;
+
+        public EditLanguageModelValidator(ILanguageService languageService)
+        {
+            _languageService = languageService ?? throw new ArgumentNullException(nameof(languageService));
+
+            RuleFor(language => language.Id)
+                .NotEmpty().WithMessage(Errors.LanguageIdCannotBeEmpty)
+                .MustAsync((id, cancellation) => _languageService.DoesLanguageAlreadyExistByIdAsync(id))
+                .WithMessage(Errors.LanguageDoesNotExist);
+
+            RuleFor(language => language)
+                .MustAsync((language, cancellation) => DoesLanguageNameIsUniqueAsync(language.Name, language.Id))
+                .WithMessage(Errors.LanguageNameIsNotUnique);
+
+            RuleFor(language => language.Name)
+                .NotEmpty().WithMessage(Errors.LanguageNameCannotBeEmpty);
+
+            RuleFor(language => language)
+                .MustAsync((language, cancellation) => DoesLanguageCodeIsUniqueAsync(language.Code, language.Id))
+                .WithMessage(Errors.LanguageCodeIsNotUnique);
+
+            RuleFor(language => language.Code)
+                .NotEmpty().WithMessage(Errors.LanguageCodeCannotBeEmpty);
+        }
+
+        private async Task<bool> DoesLanguageNameIsUniqueAsync(string languageName, Guid Id)
+        {
+            var result = await _languageService.GetLanguageIdByNameAsync(languageName);
+
+            if (result == Guid.Empty)
+            {
+                return true;
+            }
+            else if (result == Id) { return true; }
+            else return false;
+        }
+
+        private async Task<bool> DoesLanguageCodeIsUniqueAsync(string languageCode, Guid Id)
+        {
+            var result = await _languageService.GetLanguageIdByCodeAsync(languageCode);
+
+            if (result == Guid.Empty)
+            {
+                return true;
+            }
+            else if (result == Id) { return true; }
+            else return false;
+        }
+    }
+}
